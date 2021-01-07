0593636454f52bc0f6bc4ed2ffa15a9040ead83b
puspendu94 <mail2sarkar.puspendu@gmail.com>
2021-01-07 07:56:48 +0530

    first commit

---
+++ test1.js
@@ -0,0 +1,9 @@
+"use strict";
+
+["1=one", "2=two", "3=three"].forEach(alert);
+
+const userName = "puspendu";
+var age = 27;
+let myText = 'Hello world! - myTest variable', place = "India";
+[`text = ${myText}`, `name = ${userName}`, `age = ${age}`, `place = ${place}`].forEach(alert);
+
