e3990a50a35f9915d14a116bd1a8e00b9e46075b
puspendu94 <mail2sarkar.puspendu@gmail.com>
2021-01-07 10:39:19 +0530

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,9 +1,29 @@
 "use strict";
 
-["1=one", "2=two", "3=three"].forEach(alert);
+// ["1=one", "2=two", "3=three"].forEach(alert);
 
-const userName = "puspendu";
-var age = 27;
-let myText = 'Hello world! - myTest variable', place = "India";
-[`text = ${myText}`, `name = ${userName}`, `age = ${age}`, `place = ${place}`].forEach(alert);
+// const userName = "puspendu";
+// var age = 27;
+// let myText = "Hello world! - myTest variable",
+//   place = "India";
+// [
+//   `text = ${myText}`,
+//   `my name is ${userName}, age = ${age} and I am from ${place}`,
+// ].forEach(alert);
 
+
+let dob = prompt("Enter your date of birth(as ddmmyyyy)");
+function _calculateAge(birthday) {
+  var year = Number(birthday.substr(4, 4));
+  var month = Number(birthday.substr(2, 2)) - 1;
+  var day = Number(birthday.substr(0, 2));
+  var today = new Date();
+  var age = today.getFullYear() - year;
+  if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
+    age--;
+  }
+  return age;
+}
+
+age = _calculateAge(dob);
+alert(`You are ${age} years old!`);
