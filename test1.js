d55350b75e1fd15723a28662af445689986297ae
puspendu94 <mail2sarkar.puspendu@gmail.com>
2021-01-08 11:19:57 +0530

    c6

--- test1.js
+++ test1.js
@@ -99,3 +99,12 @@ function add_item() {
     alert("plz add a value to item");
   }
 }
+
+const setBg = () => {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = "#" + randomColor;
+  //color.innerHTML = "#" + randomColor;
+};
+
+genNew.addEventListener("click", setBg);
+setBg();
